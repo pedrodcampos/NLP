3e69065eb7079175b406fdb442047e882b37c63b
Pedro Campos <pedro.campos@trilogy.com>
2019-02-13 12:35:00 -0200

    added stopwords to initialization

--- clustering/stacey/__init__.py
+++ clustering/stacey/__init__.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import tqdm
 from . import cleaning
+from . import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.stem import SnowballStemmer
 
